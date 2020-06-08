3867489d733fcd6953987cb6ebbb2b1f2a651b06
María Cristina <58868315+mariacristinatovarsalas@users.noreply.github.com>
2020-06-08 17:27:51 -0300

    Updates

---
+++ precache-manifest.87b9086c97f582620a777c4cd2e391c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dffd188785d2321464f08019617978d",
+    "url": "/gorditos-community-spa-react/index.html"
+  },
+  {
+    "revision": "71dd587b6cb3c8a29e3f",
+    "url": "/gorditos-community-spa-react/static/css/main.92843d30.chunk.css"
+  },
+  {
+    "revision": "2ce73e0215ff16636451",
+    "url": "/gorditos-community-spa-react/static/js/2.ed010d64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gorditos-community-spa-react/static/js/2.ed010d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71dd587b6cb3c8a29e3f",
+    "url": "/gorditos-community-spa-react/static/js/main.4f1e8116.chunk.js"
+  },
+  {
+    "revision": "46dcdd20eb50bff92786",
+    "url": "/gorditos-community-spa-react/static/js/runtime-main.b2da5f5a.js"
+  },
+  {
+    "revision": "3c2ea5fc85ccb39ef09a328c2336b7c8",
+    "url": "/gorditos-community-spa-react/static/media/food.3c2ea5fc.jpg"
+  },
+  {
+    "revision": "2429134abc2bc517e0bfc6f72ddeb242",
+    "url": "/gorditos-community-spa-react/static/media/header-img.2429134a.jpg"
+  },
+  {
+    "revision": "babcb83197f9fd1f291bf0b23a45c42b",
+    "url": "/gorditos-community-spa-react/static/media/logo.babcb831.png"
+  }
+]);
